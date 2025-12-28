360e5779b2305a57b624e3ae730adceaa4d89ed4
PeterCavv <pedroabad52@gmail.com>
2025-12-28 15:11:38 +0100

    Commit Inicial

---
+++ src/main/java/com/dataproject/yorha/YorhaApplication.java
@@ -0,0 +1,13 @@
+package com.dataproject.yorha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YorhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YorhaApplication.class, args);
+	}
+
+}
