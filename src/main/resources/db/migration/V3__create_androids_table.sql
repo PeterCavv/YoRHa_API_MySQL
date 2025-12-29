43d6956db7e8e2f3cb6266cdece29bdc55f43a56
PeterCavv <pedroabad52@gmail.com>
2025-12-29 18:09:26 +0100

    Eliminadas migraciones tras cambios en relaciones

--- src/main/resources/db/migration/V3__create_androids_table.sql
+++ src/main/resources/db/migration/V3__create_androids_table.sql
@@ -1,11 +1,12 @@
 CREATE TABLE `androids` (
   `id` integer PRIMARY KEY AUTO_INCREMENT,
   `number` integer NOT NULL,
-  `status` enum NOT NULL,
+  `status` varchar(150) NOT NULL,
   `description` longtext,
   `model` varchar(120) NOT NULL,
   `appearance` varchar(100) NOT NULL,
   `type_id` integer,
+  `operator_id` integer NULL,
   `created_at` timestamp,
   `updated_at` timestamp,
   `deleted_at` timestamp
@@ -14,3 +15,8 @@ CREATE TABLE `androids` (
 ALTER TABLE `androids`
 ADD FOREIGN KEY (`type_id`)
 REFERENCES `types` (`id`);
+
+ALTER TABLE `androids`
+ADD CONSTRAINT fk_operator
+FOREIGN KEY (operator_id)
+REFERENCES androids(id);
